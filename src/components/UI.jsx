33ccc300860c85138700e58167a986859984bf1e
ammriteshh <amritesh6767@gmail.com>
2025-06-08 00:14:45 +0530

    commit

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -7,7 +7,7 @@ const pictures = [
   "bhu",
   "rumi-gate",
   "clock-tower",
-  "vishwanath-temple",
+  "kashi-vishwanath",
   "fatehpur-sikri",
   "imambada",
   "amu",
@@ -91,53 +91,8 @@ export const UI = () => {
             <h1 className="shrink-0 text-white text-10xl font-black ">
               Uttar Pradesh Tourism 3D Magazine
             </h1>
-            {/* <h2 className="shrink-0 text-white text-8xl italic font-light">
-              React Three Fiber
-            </h2>
-            <h2 className="shrink-0 text-white text-12xl font-bold">
-              Three.js
-            </h2>
-            <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              Ultimate Guide
-            </h2>
-            <h2 className="shrink-0 text-white text-9xl font-medium">
-              Tutorials
-            </h2>
-            <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              Learn
-            </h2>
-            <h2 className="shrink-0 text-white text-13xl font-bold">
-              Practice
-            </h2>
-            <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
-              Creative
-            </h2> */}
           </div>
           <div className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max">
-            <h1 className="shrink-0 text-white text-10xl font-black ">
-              Wawa Sensei
-            </h1>
-            <h2 className="shrink-0 text-white text-8xl italic font-light">
-              {/* React Three Fiber */}
-            </h2>
-            <h2 className="shrink-0 text-white text-12xl font-bold">
-              {/* Three.js */}
-            </h2>
-            <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              {/* Ultimate Guide */}
-            </h2>
-            <h2 className="shrink-0 text-white text-9xl font-medium">
-              {/* Tutorials */}
-            </h2>
-            <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              {/* Learn */}
-            </h2>
-            <h2 className="shrink-0 text-white text-13xl font-bold">
-              {/* Practice */}
-            </h2>
-            <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
-              {/* Creative */}
-            </h2>
           </div>
         </div>
       </div>
